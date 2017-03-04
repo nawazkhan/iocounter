256f3415df48c7a52ed00f8422dd44cf1d282536
Nawazkhan <nawazkhan@stubhub.com>
2017-03-04 14:35:08 -0800

    added static folder and files

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express'),
 	port = process.env.PORT || 3000,
 	app = express();
 
+
+
 app.get('/', function(request, response) {
-	response.sendfile(__dirname + '/iocounter.html');
+	response.sendFile(__dirname + '/iocounter.html');
 }).listen(port);
+
+app.use(express.static('js')); //makes anything inside js public
